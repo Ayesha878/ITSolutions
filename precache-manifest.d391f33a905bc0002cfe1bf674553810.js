6e94c0a416a3192a21ea8901df75f8870e6ada82
Ayesha878 <ayeshaaqeel878@yahoo.com>
2020-09-12 00:07:14 +0500

    Updates

---
+++ precache-manifest.d391f33a905bc0002cfe1bf674553810.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e072f4b2727cfe1d847ab6f97ed255a1",
+    "url": "/ITSolutions/index.html"
+  },
+  {
+    "revision": "5243559c443448917874",
+    "url": "/ITSolutions/static/css/main.93b6809f.chunk.css"
+  },
+  {
+    "revision": "3b9fd45436288e01603a",
+    "url": "/ITSolutions/static/js/2.d2874325.chunk.js"
+  },
+  {
+    "revision": "8bb9da2e92887b18f123da5413bfe448",
+    "url": "/ITSolutions/static/js/2.d2874325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5243559c443448917874",
+    "url": "/ITSolutions/static/js/main.2bae20e9.chunk.js"
+  },
+  {
+    "revision": "6c9e00486f7f9e050830",
+    "url": "/ITSolutions/static/js/runtime-main.ef24e8d3.js"
+  },
+  {
+    "revision": "cf5b83d4a8ed595e170bea15ab420773",
+    "url": "/ITSolutions/static/media/About_pic2.cf5b83d4.jpg"
+  },
+  {
+    "revision": "e57168b69ffdbc888644baf5363a1909",
+    "url": "/ITSolutions/static/media/workplace.e57168b6.jpg"
+  }
+]);
